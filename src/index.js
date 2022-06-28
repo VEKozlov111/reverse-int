2293d70ecb7332bf6aae8c113ed48470fb1d3307
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2022-06-28 15:51:47 +0300

    not all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = String(n);
+    let array = str.split('');
+    if (array[0] == '-') {
+        let a = array.shift();
+        let b = a.reverse();
+        let newN = b.join('');
+        return Number(newN);
+    } else {
+        let reverseArray = array.reverse();
+        let newN = reverseArray.join('');
+        return Number(newN);
+    };
+
 }
